ef7be0dc7ee0974c2039e5385fab7a9e9eafced5
Liuhui <2251683230@qq.com>
2019-04-17 19:22:02 +0800

    引入react-redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import router from './router';
-ReactDOM.render(router, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import store from './store';
+ReactDOM.render(<Provider store={store}>{router}</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
